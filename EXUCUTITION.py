c78e62a013c5ed9d1b713ad6193e5d5f44a152e9
gd140823 <gujjuladurgareddy@gmail.com>
2025-03-28 15:25:05 +0530

    Add files via upload

---
+++ EXUCUTITION.py
@@ -0,0 +1,66 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:\Users\Akshaya\OneDrive\Desktop\ATM CONSOLE STATMENTS.py =====
+Welcome!
+***********************************
+1. Withdrawal
+2. Deposit
+3. PIN Generation
+4. Mini Statement
+5. Exit
+Enter your option: 1
+Enter account number: 1001
+Enter PIN: 1429
+Enter amount to withdraw: 5000
+Withdrawal successful!
+***********************************
+1. Withdrawal
+2. Deposit
+3. PIN Generation
+4. Mini Statement
+5. Exit
+Enter your option: 2
+Enter account number: 1002
+Enter amount to be deposited: 2000
+Deposit successful!
+***********************************
+1. Withdrawal
+2. Deposit
+3. PIN Generation
+4. Mini Statement
+5. Exit
+Enter your option: 3
+Enter account number: 1003
+Enter PIN: 2003
+Re-enter PIN: 2003
+PIN generated successfully!
+***********************************
+1. Withdrawal
+2. Deposit
+3. PIN Generation
+4. Mini Statement
+5. Exit
+Enter your option: 4
+Enter account number: 1001
+Enter PIN: 1429
+Account Number: 1001
+Name: user 1
+Date of Birth: 14-08-25
+Balance: 5000
+***********************************
+1. Withdrawal
+2. Deposit
+3. PIN Generation
+4. Mini Statement
+5. Exit
+Enter your option: 2
+Enter account number: 1001
+Enter amount to be deposited: 20000
+Deposit successful!
+***********************************
+1. Withdrawal
+2. Deposit
+3. PIN Generation
+4. Mini Statement
+5. Exit
